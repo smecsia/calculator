0f6bd623277978bdfd623954afcb03cb0088438e
Ilya Sadykov <smecsia@gmail.com>
2013-03-24 22:19:34 +0400

    evaluator, generated code, etc...

--- backend/src/main/java/me/smecsia/testtask/calculator/backend/to/calcresult/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://calcresult", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package me.smecsia.testtask.calculator.backend.to.calcresult;
--- backend/src/main/java/me/smecsia/testtask/calculator/backend/to/calculator/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://calculator", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package me.smecsia.testtask.calculator.backend.to.calculator;
